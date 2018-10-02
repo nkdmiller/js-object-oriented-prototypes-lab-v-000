4c995be14a6cef8358db14e9a4ad801cbfb865b9
Nate Miller <natekmiller96@gmail.com>
2018-10-02 05:11:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,12 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function() {
    return `No, I must disagree`;
 };
+
+BoardMember.prototype.approve = function() {
+   return `You can do that!`;
+};
+
+BoardMember.prototype.doCharity = function() {
+   return `I like to help people`;
+};
  
