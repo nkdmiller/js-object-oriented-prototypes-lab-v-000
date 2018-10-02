41b250235dea924b9bfedd00892a25fde6c13b83
Nate Miller <natekmiller96@gmail.com>
2018-10-02 05:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ BoardMember.prototype.releasePressStatement = function(){
   return "You will see great things from Scuber.";
 };
 
-releasePressStatement = function(){
+BoardMember.prototype.sayHi = function(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 };
